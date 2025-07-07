9ea5f093536a22fb801538d2ff93bc7d15bb70d0
Facusangoy <sangoyfacundog@gmail.com>
2025-07-07 17:52:01 -0300

    Pagina

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,45 @@
-from django.shortcuts import render
-from .models import Post
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import EditUserForm, AvatarForm
+from .models import Avatar
 
-def index(request):
-    posts = Post.objects.all()
-    return render(request, 'AppBlog/index.html', {'posts': posts})
+@login_required
+def editar_perfil(request):
+    if request.method == "POST":
+        form = EditUserForm(request.POST, instance=request.user)
 
-def detalle_post(request, id):
-    post = Post.objects.get(pk=id)
-    return render(request, 'AppBlog/detalle_post.html', {'post': post})
+        # Verificar si el usuario tiene un avatar
+        try:
+            avatar = request.user.avatar
+        except Avatar.DoesNotExist:
+            avatar = None
+
+        # Crear el formulario de avatar según si el usuario tiene uno o no
+        if avatar:
+            avatar_form = AvatarForm(request.POST, request.FILES, instance=avatar)
+        else:
+            avatar_form = AvatarForm(request.POST, request.FILES)
+
+        if form.is_valid() and avatar_form.is_valid():
+            form.save()
+            avatar_instance = avatar_form.save(commit=False)
+            avatar_instance.user = request.user  # Asignar el usuario actual al avatar
+            avatar_instance.save()
+            return redirect("Main:perfil")
+    else:
+        form = EditUserForm(instance=request.user)
+
+        try:
+            avatar = request.user.avatar
+        except Avatar.DoesNotExist:
+            avatar = None
+
+        if avatar:
+            avatar_form = AvatarForm(instance=avatar)
+        else:
+            avatar_form = AvatarForm()
+
+    return render(request, "editar_perfil.html", {
+        "form": form,
+        "avatar_form": avatar_form
+    })
